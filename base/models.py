fbb47adcd3d27e44ad9879b1a23b6eb75618e0b4
lionheart <mikeoladapo2004@gmail.com>
2024-12-21 00:30:38 +0100

    commit

--- base/models.py
+++ base/models.py
@@ -1,5 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Room(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
+
+class Message(models.Model):
+    room = models.ForeignKey(Room,related_name="messages",on_delete=models.CASCADE)
+    user = models.ForeignKey(User,related_name="messages",on_delete=models.CASCADE)
