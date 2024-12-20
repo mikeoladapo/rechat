a7936b0275bcdd2a6dd632c9968cbec6ffbe0267
lionheart <mikeoladapo2004@gmail.com>
2024-12-21 00:16:32 +0100

    commit

--- base/consumers.py
+++ base/consumers.py
@@ -1,5 +1,6 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
+import json
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -15,4 +16,29 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.room_group_name,self.channel_layer
         )
     
-    async def send(self,text)
+    async def receive(self,text_data):
+        data = json.loads(text_data)
+        message = data["message"]
+        sender = data["username"]
+        room = data["room"]
+
+        await self.channel_layer.group_send(
+            self.room_group_name,{
+                "type":"chat_message",
+                "message":message,
+                "room":room,
+                "sender":sender
+            }
+        )
+
+    async def chat_message(self,event):
+        message = event["message"]
+        sender = event["username"]
+        room = event["room"]
+        await self.send(
+            text_data=json.dumps({
+                "message":message,
+                "room":room,
+                "sender":sender
+            })
+        )
